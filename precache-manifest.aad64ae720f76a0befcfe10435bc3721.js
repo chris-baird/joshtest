da137c4b2cc6cdb004f7fdff0195e5fbf3521b84
Chris <chrisbaird05@gmail.com>
2020-01-11 11:10:35 -0800

    changed files

---
+++ precache-manifest.aad64ae720f76a0befcfe10435bc3721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a9ef7891baa068631ce763346996cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c70bb172f87db8bcb5f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "002b0eed44d65e93ffea",
+    "url": "/static/js/2.3027922f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3027922f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c70bb172f87db8bcb5f",
+    "url": "/static/js/main.65aa1cf9.chunk.js"
+  },
+  {
+    "revision": "e936cf5467605e120a67",
+    "url": "/static/js/runtime-main.81dd7862.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
